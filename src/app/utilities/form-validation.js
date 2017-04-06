c44cf72d8e0369678a9127f9b4b75641e260e02c
Jan Chalupa <jan.chalupa@actum.cz>
2017-04-06 14:04:58 +0200

    no message

--- src/app/utilities/form-validation.js
+++ src/app/utilities/form-validation.js
@@ -1,8 +1,11 @@
+import axios from 'axios';
+
 const formValidation = (form) => {
     const messagesElement = form.querySelectorAll('.rules-info .message');
-    let inputElements = form.querySelectorAll('input:not([type="radio"])'); // To-do: Add .form-check
     const knownRules = ['min-length', 'max-length', 'required', 'pattern'];
     const messages = [];
+    const submitButton = form.querySelector('.submit-button');
+    let inputElements = form.querySelectorAll('input:not([type="radio"])'); // To-do: Add .form-check
 
     Array.from(inputElements).forEach((element) => {
         element.classList.add('validating');
@@ -12,11 +15,20 @@ const formValidation = (form) => {
     inputElements = form.querySelectorAll('.validating');
 
     Array.from(inputElements).forEach((element) => {
-        const rules = parseRules(element.dataset.rules);
-
         element.addEventListener('keyup', (e) => {
-            validate(e.target, rules);
+            validateInput(e.target);
         });
+
+        if (element.type === 'date') {
+            element.addEventListener('change', (e) => {
+                validateInput(e.target);
+            });
+        }
+    });
+
+    submitButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        submitForm();
     });
 
     Array.from(messagesElement).forEach((element) => {
@@ -24,84 +36,101 @@ const formValidation = (form) => {
         const message = element.innerHTML;
 
         messages.push({
-            name: name,
-            message: message
+            name,
+            message
         });
     });
 
-    function validate(element = null, rules = null) {
-        if (element && rules) {
-            const value = element.value;
-            const errors = [];
+    validateAll();
 
-            Array.from(rules).forEach((rule) => {
-                const patt1 = /^\/.*\/[a-z]{0,3}$/gim;
-                let missingRule = true;
+    function validateAll() {
+        Array.from(inputElements).forEach((element) => {
+            validateInput(element);
+        });
+    }
 
-                if (rule.name === 'format') {
-                    const pattern = rule.value.substring(1, rule.value.length - 1);
+    function validateInput(element) {
+        const rules = parseRules(element.dataset.rules);
+        const value = element.value;
+        const errors = [];
 
-                    if (!new RegExp(pattern).test(value)) {
-                        errors.push({
-                            name: rule.name,
-                            required: 'wrong format',
-                            current: 'right format'
-                        });
+        Array.from(rules).forEach((rule) => {
+            let missingRule = true;
 
-                        missingRule = false;
-                    }
+            if (rule.name === 'format') {
+                const pattern = rule.value.substring(1, rule.value.length - 1);
+
+                if (!new RegExp(pattern).test(value)) {
+                    errors.push({
+                        name: rule.name,
+                        required: null,
+                        current: null
+                    });
                 }
 
-                if (rule.name === 'min-length' && value.length < rule.value) {
+                missingRule = false;
+            }
+
+            if (rule.name === 'min-length') {
+                if (value.length < rule.value) {
                     errors.push({
                         name: rule.name,
                         required: rule.value,
                         current: value.length
                     });
-
-                    missingRule = false;
                 }
 
-                if (rule.name === 'max-length' && value.length > rule.value) {
+                missingRule = false;
+            }
+
+            if (rule.name === 'max-length') {
+                if (value.length > rule.value) {
                     errors.push({
                         name: rule.name,
                         required: rule.value,
                         current: value.length
                     });
-
-                    missingRule = false;
                 }
 
-                if (rule.name === 'required' && value.length === 0) {
+                missingRule = false;
+            }
+
+            if (rule.name === 'required') {
+                if (value.length === 0) {
                     errors.push({
                         name: rule.name,
-                        required: 'filled',
-                        current: 'empty'
+                        required: null,
+                        current: null
                     });
-
-                    missingRule = false;
                 }
 
-                if (missingRule) {
-                    throw new Error(`Unknown rule name: ${rule.name}`);
+                missingRule = false;
+            }
+
+            if (missingRule) {
+                throw new Error(`Unknown rule name: '${rule.name}'. List of known rules: ${knownRules.join(', ')}`);
+            }
+        });
+
+        let html = '';
+        Array.from(errors).forEach((error) => {
+            Array.from(messages).forEach((message) => {
+                if (error.name === message.name) {
+                    html += `<div class="error">${fillErrorPlaceholders(error, message.message)}</div>`;
                 }
             });
+        });
 
-            let html = '';
-            Array.from(errors).forEach((error) => {
-                Array.from(messages).forEach((message) => {
-                    if (error.name === message.name) {
-                        html += `<div class="error">${fillErrorPlaceholders(error, message.message)}</div>`;
-                    }
-                });
-            });
-
-            element.parentElement.querySelector('.errors').innerHTML = html;
-        }
+        element.parentElement.querySelector('.errors').innerHTML = html;
+        submitButton.disabled = !isValid();
     }
 
     function fillErrorPlaceholders(error, message) {
         return message.replace('@{current}', error.current).replace('@{required}', error.required);
+    }
+
+    function isValid() {
+        return form.querySelectorAll('.errors .error').length === 0;
     }
 
     function parseRules(dataRules) {
@@ -117,12 +146,12 @@ const formValidation = (form) => {
                     const value = dataRule[1].trim();
 
                     rules.push({
-                        name: name,
+                        name,
                         value: setDatatype(value)
                     });
                 } else if (dataRule.length === 1) {
                     rules.push({
-                        name: name,
+                        name,
                         value: true
                     });
                 } else {
@@ -130,7 +159,7 @@ const formValidation = (form) => {
                 }
             });
 
-            return checkUnknownRules(rules);
+            return rules;
         }
 
         return null;
@@ -146,28 +175,42 @@ const formValidation = (form) => {
         return value;
     }
 
-    function checkUnknownRules(rules) {
-        /* const filteredRules = {};
+    function submitForm() {
+        const data = getFormData();
 
-        Array.from(Object.keys(rules)).forEach((key) => {
-            if (knownRules.indexOf(key) > -1) {
-                filteredRules[key] = rules[key];
-            }
+        axios('', {
+            method: 'POST',
+            params: data
+        })
+        .then((response) => {
+            alert('Data posted.');
+        })
+        .catch((error) => {
+            throw new Error(error);
         });
-
-        return filteredRules; */
-
-        return rules;
     }
 
-    /* function makeCamelCase(value) {
+    function getFormData() {
+        const data = {};
+
+        Array.from(inputElements).forEach((element) => {
+            const key = makeCamelCase(element.id);
+            const value = element.value;
+
+            data[key] = value;
+        });
+
+        return data;
+    }
+
+    function makeCamelCase(value) {
         value = value.toLowerCase();
         value = value.replace(/[-_]/g, ' ');
         value = value.split(/\s/g);
 
         let camel = value[0];
 
-        for (let i = 1; i < value.length; i++) {
+        for (let i = 1; i < value.length; i += 1) {
             camel += capitalize(value[i]);
         }
 
@@ -176,7 +219,7 @@ const formValidation = (form) => {
 
     function capitalize(value) {
         return value.charAt(0).toUpperCase() + value.slice(1);
-    } */
+    }
 };
 
 export default formValidation;
